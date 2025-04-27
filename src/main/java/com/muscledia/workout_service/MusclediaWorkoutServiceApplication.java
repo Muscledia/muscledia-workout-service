63398f72d8a56adecd790a80be38fb330d176cd2
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-04-27 19:43:18 +0200

    initial commit

---
+++ src/main/java/com/muscledia/workout_service/MusclediaWorkoutServiceApplication.java
@@ -0,0 +1,13 @@
+package com.muscledia.workout_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusclediaWorkoutServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusclediaWorkoutServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muscledia/workout_service/MusclediaWorkoutServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muscledia.workout_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusclediaWorkoutServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
