73d62a161ace0410743cf16d139e52a33ebb588b
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-06-04 17:59:33 +0200

    feat(exercise): Add ExerciseDataContainer DTO to map API 'data' object

---
+++ src/main/java/com/muscledia/workout_service/external/exercisedb/dto/ExerciseDataContainer.java
@@ -0,0 +1,19 @@
+package com.muscledia.workout_service.external.exercisedb.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ExerciseDataContainer {
+
+    private String previousPage; // Can be null
+    private String nextPage;     // Can be null
+    private int totalPages;
+    private int totalExercises;
+    private int currentPage;
+
+    @JsonProperty("exercises") // Maps the "exercises" array to a List of ExerciseData
+    private List<ExerciseData> exercises;
+}
