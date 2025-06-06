315402905ac04491c98aa4d7a774eecacd8a95fa
Eric Muganga <105128548+eric-muganga@users.noreply.github.com>
2025-06-06 23:58:49 +0200

    feat(dto): Create ExerciseApiFullResponse DTO for full API top-level response

--- src/main/java/com/muscledia/workout_service/external/exercisedb/dto/ExerciseApiResponse.java
+++ src/main/java/com/muscledia/workout_service/external/exercisedb/dto/ExerciseApiResponse.java
@@ -1,9 +1,6 @@
 package com.muscledia.workout_service.external.exercisedb.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-
-import java.util.List;
 
 @Data
 public class ExerciseApiResponse {
